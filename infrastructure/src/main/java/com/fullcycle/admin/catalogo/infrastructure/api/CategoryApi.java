e99ba3007469354e42a75234686e56075a2f8db3
carvalho <carvalho.pedro.cardoso@gmail.com>
2023-11-04 01:06:20 -0300

    lint

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryApi.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/CategoryApi.java
@@ -1,33 +1,24 @@
 package com.fullcycle.admin.catalogo.infrastructure.api;
 
-import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "categories")
 public interface CategoryApi {
 
-    @PostMapping(
-            consumes = APPLICATION_JSON_VALUE,
-            produces = APPLICATION_JSON_VALUE
-    )
-    @ResponseStatus(value = CREATED)
-    ResponseEntity<?> createCategory();
+  @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+  @ResponseStatus(value = CREATED)
+  ResponseEntity<?> createCategory();
 
-    @GetMapping
-    Pagination<?> listCategories(
-            @RequestParam(name = "search",required = false,defaultValue = "") final String search,
-            @RequestParam(name = "page",required = false,defaultValue = "0") final int page,
-            @RequestParam(name = "perPage",required = false,defaultValue = "10") final int perPage,
-            @RequestParam(name = "sort",required = false,defaultValue = "name") final String sort,
-            @RequestParam(name = "dir",required = false,defaultValue = "asc") final String direction
-
-
-    );
-
+  @GetMapping
+  Pagination<?> listCategories(
+      @RequestParam(name = "search", required = false, defaultValue = "") final String search,
+      @RequestParam(name = "page", required = false, defaultValue = "0") final int page,
+      @RequestParam(name = "perPage", required = false, defaultValue = "10") final int perPage,
+      @RequestParam(name = "sort", required = false, defaultValue = "name") final String sort,
+      @RequestParam(name = "dir", required = false, defaultValue = "asc") final String direction);
 }
--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController implements CategoryApi {
-    @Override
-    public ResponseEntity<?> createCategory() {
-        return null;
-    }
+  @Override
+  public ResponseEntity<?> createCategory() {
+    return null;
+  }
 
-    @Override
-    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String direction) {
-        return null;
-    }
+  @Override
+  public Pagination<?> listCategories(
+      String search, int page, int perPage, String sort, String direction) {
+    return null;
+  }
 }
