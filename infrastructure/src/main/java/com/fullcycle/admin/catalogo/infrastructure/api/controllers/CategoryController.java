03805e6746b0b6df9efa3c9153939c9139c53555
carvalho <carvalho.pedro.cardoso@gmail.com>
2023-11-04 01:06:06 -0300

    create interface CategoryApi

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.infrastructure.api.CategoryApi;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController implements CategoryApi {
+    @Override
+    public ResponseEntity<?> createCategory() {
+        return null;
+    }
+
+    @Override
+    public Pagination<?> listCategories(String search, int page, int perPage, String sort, String direction) {
+        return null;
+    }
+}
